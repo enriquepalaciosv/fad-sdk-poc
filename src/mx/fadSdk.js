42e22560ce59b259ee5dbb048b8e46f28ee3fd23
enriquepalaciosv <palacios335@gmail.com>
2025-06-11 15:31:55 -0600

    Implement selfie verification

--- src/mx/fadSdk.js
+++ src/mx/fadSdk.js
@@ -2,13 +2,12 @@ import FadSDK from "@fad-producto/fad-sdk";
 
 export function getSdkInstance() {
   // TODO: set environment dynamically
-  const TOKEN = "WVFDSTdGclFBWEt4TytScXpNS1NtdGZtMnhBQU1FWEdkSzMrNFpMMmFVaHR2Mk4xYTVhdmQxS0cvTm5BV2VUbzBVemdOMEQ4UVhyRjV1elFsMFJEaFNXWTBjWTRBSStrMG5IZDQzUjZRZEtVMFBkaWJkOXFQZFBsYUdsbmQram0=";
+  const TOKEN =
+    "WVFDSTdGclFBWEt4TytScXpNS1NtdGZtMnhBQU1FWEdkSzMrNFpMMmFVaHR2Mk4xYTVhdmQxS0cvTm5BV2VUbzBVemdOMEQ4UVhyRjV1elFsMFJEaFNXWTBjWTRBSStrMG5IZDQzUjZRZEtVMFBkaWJkOXFQZFBsYUdsbmQram0=";
   const options = {
     environment: FadSDK.getFadEnvironments().UAT,
   };
   return new FadSDK(TOKEN, options);
 }
 
-export function getFacetecCredentials() {
-    
-}
+export const FACETEC_PROCESS_TYPE = FadSDK.Constants.Facetec.ProcessType.OTHER;
